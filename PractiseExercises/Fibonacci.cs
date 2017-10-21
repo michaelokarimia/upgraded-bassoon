b0eb1cd134d2d77a3184debfe2992f43f0a446cd
Michael Okarimia <git@michaelokarimia.com>
2017-10-21 21:09:07 +0100

    Get the nth number from the Fibonacci sequence

---
+++ PractiseExercises/Fibonacci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PractiseExercises
+{
+    public class Fibonacci
+    {
+        internal static int Print(int n)
+        {
+            return Run(n);
+        }
+
+
+        internal static int Run(int n)
+        {
+            if (n < 2)
+                return n;
+
+            var list = new List<int>() { 0, 1 };
+
+            for (int i = 2; i < n; i++)
+            {
+                list.Add(list[i - 1] + list[i - 2]);
+            }
+
+            return list[n-1];
+        }
+    }
+}
--- PractiseExercises/Program.cs
+++ PractiseExercises/Program.cs
@@ -10,6 +10,12 @@ namespace PractiseExercises
     {
         static void Main(string[] args)
         {
+
+            int n = 9;
+
+            Console.WriteLine("The Fibbonacci Number {0} is {1}", n, Fibonacci.Print(n));
+
+            Console.ReadKey();
         }
     }
 }
