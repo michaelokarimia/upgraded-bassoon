5e3cd98753ac1f8f7fb943f3673b592862557978
Michael Okarimia <git@michaelokarimia.com>
2017-10-22 13:11:09 +0100

    Fix to make stock comparisions time ordered O(n^2)
    Only compares early stock prices with later ones when calculating
    potential profit

--- PractiseExercises/StockPriceProfitCalculator.cs
+++ PractiseExercises/StockPriceProfitCalculator.cs
@@ -10,8 +10,9 @@ namespace PractiseExercises
     {
         internal static void Run()
         {
-            // price of Apple stock yesterday in Dollars
-            var stockPricesYesterday = new int[] { 10, 7, 5, 8, 11, 9};
+            // price of Apple stock yesterday in Dollars, in time order
+            //each element represents a minute since market open time, value = price
+            var stockPricesYesterday = new int[] { 100, 10, 7, 5, 8, 11, 9};
 
             int profit = 0;
 
@@ -21,28 +22,42 @@ namespace PractiseExercises
         }
 
         //Given the stock prices yesterday, return the max profit that could have been made with one purchase and one sale of a share
+        //Order sensitive, can only compare prices with future increase.
+        //assumes we do not return a negative number 9 i.e. a loss
         private static int getMaxProfit(int[] stockPricesYesterday)
         {
-            var highest = 0;
-            var lowest = int.MaxValue;
+            if( stockPricesYesterday == null)
+            {
+                throw new Exception("Null Array passed in");
+            }
+
+            if (stockPricesYesterday.Length < 2)
+            {
+                throw new Exception("Invalid list of prices passed in, too few prices");
+            }
+
+            var maxProfit = 0;
 
             for (int i = 0; i < stockPricesYesterday.Length; i++)
             {
                 var current = stockPricesYesterday[i];
-                if (current > highest)
+                for (int n = i+1; n < stockPricesYesterday.Length; n ++)
                 {
-                    highest = current;
-                }
-                if (current < lowest)
-                {
-                    lowest = current;
+                    var nextPrice = stockPricesYesterday[n];
+
+                    var potentialProfit = nextPrice - current;
+
+                    if (potentialProfit > 0 && potentialProfit > maxProfit)
+                    {
+                        maxProfit = potentialProfit;
+                    }
+
                 }
             }
 
 
-            var profit = highest - lowest;
 
-            return profit;
+            return maxProfit;
         }
     }
 }
