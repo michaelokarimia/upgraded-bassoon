999fe7922a2dee0e7c515f7a666f1ef28185a0a2
Michael Okarimia <git@michaelokarimia.com>
2017-10-25 16:12:55 +0100

    Annotated with explanations

--- PractiseExercises/K_ListsSmallestRange.cs
+++ PractiseExercises/K_ListsSmallestRange.cs
@@ -14,8 +14,8 @@ namespace PractiseExercises
             var input = new List<List<int>>()
             {
                 new List<int> { 4, 10, 15, 24, 26},
-                new List<int> { 0 ,9 , 12, 20 },
-                new List<int> { 5, 18, 22, 30}
+                new List<int> { 0 , 9 , 12, 20 },
+                new List<int> { 5, 18, 22,30}
             };
 
             var outputList = GetSmallestRange(input);
@@ -25,7 +25,10 @@ namespace PractiseExercises
             }
         }
 
-        //represents a value at an index of a lists. Can iterate along the list for all indexes
+        //Decorates a k list to allow traversal, and for it to remember its current index
+        //holds it's current index for the list
+        //Value returns the value at list[currentIndex]
+        //moveNext() increments currentIndex and returns currentIndex, or NULL if currentIndex can not be incremented(end of list)
         public class RangeList
         {
             public RangeList(List<int> array)
@@ -40,7 +43,8 @@ namespace PractiseExercises
                 return list[currentIndex];
             }
 
-            //iterate the index of the list, if there are more elements, otherwise return null
+            //increment the currentindex of the list, if there are more elements, otherwise return null
+            //can be used to check if current list if at the end or not
             public int? moveNext()
             {
                 if (currentIndex == list.Count - 1)
@@ -49,7 +53,6 @@ namespace PractiseExercises
                 currentIndex++;
 
                 return currentIndex;
-
             }
 
             public List<int> list { get; }
@@ -64,10 +67,9 @@ namespace PractiseExercises
             var minDiffRange = int.MaxValue;
    
 
-            //data structure to hold the range of elements in every list
             List<RangeList> rangeLists = new List<RangeList>();
-
-            //for each list add it a RangeListArray
+            //Rangelist decorates each k lists with a current index and an iterator that allows for traversal
+            //for each list add it a RangeList lists
             foreach (List<int> list in input)
             {
                 rangeLists.Add(new RangeList(list));
@@ -77,32 +79,45 @@ namespace PractiseExercises
 
             while (isPossibleNext)
             {
+                //represents that elements are being compared and need their range calculated
                 List<WorkingList> currentWorkingList = new List<WorkingList>();
 
-                //iterate through each rangelist and add them to a workingList
+                //load up the workingList
                 foreach (RangeList rl in rangeLists)
                 {
                     currentWorkingList.Add(new WorkingList( rl.Value(), rl));
                 }
+                
+                //now to calculate the candidate range between the lists.
 
+
+                //sort by value of the smallest current val first. Only care about the list with the smallest val
+                //sorting this way means we only increment the list with the lowest value
                 currentWorkingList.Sort();
 
-                var first = currentWorkingList[0].val;
-                var last = currentWorkingList[currentWorkingList.Count - 1].val;
-                var potentialmin = last - first + 1;
+                //get the smallest number of all the lists
+                var smallest = currentWorkingList[0].val;
 
+                //since this has been sorted, get the largest number of the lists
+                var largest = currentWorkingList[currentWorkingList.Count - 1].val;
+
+                var potentialmin = largest - smallest + 1; //zero based arrays, so add one each time.
+                
+                //if this range is smaller than the last known smallest range, replace it and store the values 
                 if(potentialmin < minDiffRange)
                 {
                     minDiffRange = potentialmin;
-                    smallestRange = new int[] { first, last };
+                    smallestRange = new int[] { smallest, largest };
                 }
-
+                //if there is another value in the list then iterate, otherwise we are done so exit loop
+                //if there is no more items in the list with the smallest number, then we're done
                 isPossibleNext = currentWorkingList[0].list.moveNext() != null;
             }
 
             return smallestRange;
         }
 
+        //datastructure that represents the list we are comparing
         public struct WorkingList :IComparable
         {
             public WorkingList(int value, RangeList rangeList)
@@ -114,6 +129,7 @@ namespace PractiseExercises
             public RangeList list { get; }
             public int val { get; }
 
+            //allows us to sort a list of working lists by the value
             public int CompareTo(object obj)
             {
                 WorkingList comparator = (WorkingList)obj;
