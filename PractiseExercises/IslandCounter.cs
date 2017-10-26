55d4e43df69e40893eea93a12142c28d1432b304
Michael Okarimia <git@michaelokarimia.com>
2017-10-26 11:55:59 +0100

    IslandCounter, only if islands are 1x1 though!

--- PractiseExercises/IslandCounter.cs
+++ PractiseExercises/IslandCounter.cs
@@ -33,17 +33,23 @@ namespace PractiseExercises
 
     public class IslandCounter
     {
-        static int[][] ocean = new int[][] {
+        static int[][] ocean1 = new int[][] {
          new int[]{1, 1, 1, 1, 0 },
          new int[]{1, 1, 0, 1, 0 },
          new int[]{1, 1, 0, 0, 0 },
          new int[]{0, 0, 0, 0, 0 }
-        }
-        ;
+        };
+
+        static int[][] ocean2 = new int[][] {
+         new int[]{1,1,0,0,0 },
+         new int[]{1,1,0,0,0 },
+         new int[]{0,0,1,0,0 },
+         new int[]{ 0, 0, 0, 1, 1 }
+        };
 
         public static void Run()
         {
-            int islandCount = getIslandCount(ocean);
+            int islandCount = getIslandCount(ocean1);
 
             Console.WriteLine("There are {0} islands", islandCount);
 
@@ -55,12 +61,83 @@ namespace PractiseExercises
             {
                 throw new Exception("Invalid ocean dimension");
             }
+            int foundIslandCount = 0;
+
+            //traverse each element, 
+
+            //If element value = 1 it's land so check 
+            //checking North, South, East, West for 0  whihc is ocean. If true, then increment island count
 
 
-            int count = 0;
+            //nestedloop, O(n^2) complexity
+            for (int j = 0; j < ocean.Length; j++)
+            {
+                //j represents each row
+
+                //i represents each column
+                for (int i = 0; i < ocean[j].Length; i++)
+                {
+                    //it's land
+                    if (ocean[j][i].Equals(1))
+                    {
+                        bool oceanToNorth, oceanToEast, oceanToSouth, oceanToWest;
+
+                        //check north
+                        //if j = 0 then north of it will be ocean
+                        if (j == 0)
+                        {
+                            oceanToNorth = true;
+                        }
+                        else
+                        {
+                            oceanToNorth = (ocean[j - 1][i].Equals(0));
+                        }
+                        
+                        //check if ocean to east
+                        //if at easternmost edge, then it's true
+                        if (i == ocean[j].Length-1)
+                        {
+                            oceanToEast = true;
+                        }
+                        else
+                        {
+                            oceanToEast = (ocean[j][i + 1].Equals(0));
+                        }
+
+                        //check if ocean to south
+                        // if j is at the length then it is ocean
+
+                        if(j == ocean.Length -1)
+                        {
+                            oceanToSouth = true;
+                        }
+                        else
+                        {
+                            oceanToSouth = ocean[j + 1][i].Equals(0);
+                        }
+
+                        //checkwest
+                        //if i is 0 then we are at the edge and ocean is to the west
+                        if(i == 0)
+                        {
+                            oceanToWest = true;
+                        }
+                        else
+                        {
+                            oceanToWest = ocean[j][i - 1].Equals(0);
+                        }
 
 
-            return count;
+                        if(oceanToNorth && oceanToEast && oceanToSouth && oceanToWest)
+                        {
+                            foundIslandCount++;
+                        }
+                    }
+                }
+
+            }
+
+            return foundIslandCount;
         }
     }
 }
